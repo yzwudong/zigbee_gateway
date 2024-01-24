016f6570cc3d3e3b785840d87ac8e1d33acb0ded
yzwudong <108610834+yzwudong@users.noreply.github.com>
2024-01-24 18:35:09 +0800

    v0.99 source code

---
+++ src/etc.h
@@ -0,0 +1,32 @@
+void getReadableTime(String &readableTime, unsigned long beginTime);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+uint8_t temprature_sens_read();
+#ifdef __cplusplus
+}
+#endif
+uint8_t temprature_sens_read();
+
+float getCPUtemp(bool clear = false);
+
+bool hasPOE();//has poe or not ,true=has POE
+
+void zigbeeEnableBSL();
+void zigbeeRestart();
+
+void adapterModeUSB();
+void adapterModeLAN();
+
+void ledYellowToggle();
+void ledBlueToggle();
+
+void getDeviceID(char * arr);
+void writeDefultConfig(const char *path, JsonDocument& doc);
+
+void resetSettings();
+
+String hexToDec(String hexString);
+
+//#define min(a, b) ((a) < (b) ? (a) : (b))
